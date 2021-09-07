b0fb9b331ea280ef417f1ab3874cfe0f973170dc
cecilie <ceciversen@gmail.com>
2021-09-07 13:15:40 +0200

    Test

--- src/test/java/com/example/demo/BownlingScroreApplicationTests.java
+++ src/test/java/com/example/demo/BownlingScroreApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BownlingScroreApplicationTests {
+class BowlingScoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assert(1==1);
 	}
 
 }
